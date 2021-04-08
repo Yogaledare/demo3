fed0e16380692489603a0479fe4bb237dbf62f37
Yogaledare <marcus_uppstrom@hotmail.com>
2021-04-08 10:56:33 +0200

    dsa

--- UDPserver.py
+++ UDPserver.py
@@ -6,13 +6,17 @@
 
 from socket import *
 
-with socket(AF_INET, SOCK_DGRAM) as mySocket:
-    mySocket.bind(('129.16.29.51', 12000))
-    while True:
-        message, address = mySocket.recvfrom(1024)
-        print("from:", address, "received:", message)
-        print(type(message))
-        if message.decode() == "kill":
-            break
+# hej = socket(AF_INET, SOCK_DGRAM)
+
+
+
+# with socket(AF_INET, SOCK_DGRAM) as mySocket:
+#     mySocket.bind(('129.16.29.51', 12000))
+#     while True:
+#         message, address = mySocket.recvfrom(1024)
+#         print("from:", address, "received:", message)
+#         print(type(message))
+#         if message.decode() == "kill":
+#             break
 
 
