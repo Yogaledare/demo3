4d29cb8a1d3145b9ed1e2489798c175f296f06df
Yogaledare <marcus_uppstrom@hotmail.com>
2021-04-09 16:06:20 +0200

    dsa

--- UDPclient.py
+++ UDPclient.py
@@ -10,5 +10,5 @@ from socket import *
 mySocket = socket(AF_INET, SOCK_DGRAM)
 msg = input("enter: ")
 while msg:
-    mySocket.sendto(msg.encode(), ("129.16.29.51", 12000))
+    mySocket.sendto(msg.encode(), ("155.4.128.188", 12000))
     msg = input("enter: ")
